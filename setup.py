678ad48098762fa07ee3ce6123498bbdaa7c6b6f
ernest <wolf1080@yandex.ru>
2020-07-26 21:22:16 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="auto-rest-models",
+    version="0.0.1",
+    author="Ernest Ipekchyan",
+    author_email="ghost199632@gmail.com",
+    description="auto REST API for models in Django project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ErnestIpekchyan/auto-rest-models",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
