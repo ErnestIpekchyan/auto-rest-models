22db04526c77e9022fd0c6727232a53c42a29957
ernest <wolf1080@yandex.ru>
2020-07-26 14:02:51 +0300

    Dog model: add name, age fields

--- tests/models.py
+++ tests/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Dog(models.Model):
+    name = models.CharField('Имя', max_length=50)
+    age = models.PositiveSmallIntegerField('Возраст')
 
     class Meta:
         verbose_name = 'Собака'
