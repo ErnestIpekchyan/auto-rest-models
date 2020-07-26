d70e5f564b769247345cd8e077cbdfc17a54ae56
ernest <wolf1080@yandex.ru>
2020-07-26 13:23:18 +0300

    add get_queryset and get_serializer_class methods

--- auto_api/views.py
+++ auto_api/views.py
@@ -2,4 +2,9 @@ from rest_framework import viewsets
 
 
 class GenericModelViewSet(viewsets.ModelViewSet):
-    pass
+
+    def get_queryset(self):
+        pass
+
+    def get_serializer_class(self):
+        pass
