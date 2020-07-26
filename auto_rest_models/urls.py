5fadbe3e5e5a5299a5c57c4bb0ad6d4a243da3ef
ernest <wolf1080@yandex.ru>
2020-07-26 13:19:31 +0300

    remove api prefix

--- auto_rest_models/urls.py
+++ auto_rest_models/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('auto_api.urls')),
+    path('', include('auto_api.urls')),
 ]
