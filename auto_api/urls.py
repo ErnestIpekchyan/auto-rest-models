35a91d0fc83119c0385716ca4ad6a9ab03ff0a2f
ernest <wolf1080@yandex.ru>
2020-07-26 13:41:25 +0300

    register DefaultRouter

--- auto_api/urls.py
+++ auto_api/urls.py
@@ -1,4 +1,14 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from auto_api.views import GenericModelViewSet
+
+router = DefaultRouter()
+router.register(
+    r'(?P<app_name>\w+)/(?P<model_name>[a-z]+)',
+    GenericModelViewSet,
+    'models',
+)
 
 urlpatterns = [
 ]
