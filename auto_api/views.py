5db1ab77434a69972f61328ec451918dc5b28240
ernest <wolf1080@yandex.ru>
2020-07-26 13:48:09 +0300

    get model class in initial method

--- auto_api/views.py
+++ auto_api/views.py
@@ -6,6 +6,12 @@ from auto_api.serializers import get_generic_serializer
 
 class GenericModelViewSet(viewsets.ModelViewSet):
 
+    def initial(self, request, *args, **kwargs):
+        super().initial(request, *args, **kwargs)
+        app_name = self.kwargs['app_name']
+        model_name = self.kwargs['model_name']
+        self.model_class = apps.get_model(app_name, model_name)
+
     def get_queryset(self):
         app_name = self.kwargs['app_name']
         model_name = self.kwargs['model_name']
