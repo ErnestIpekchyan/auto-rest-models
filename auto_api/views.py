1debbff9c79839ccb4e51930887ff73282cf37db
ernest <wolf1080@yandex.ru>
2020-07-26 13:32:51 +0300

    get queryset from model class

--- auto_api/views.py
+++ auto_api/views.py
@@ -1,10 +1,14 @@
+from django.apps import apps
 from rest_framework import viewsets
 
 
 class GenericModelViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
-        pass
+        app_name = self.kwargs['app_name']
+        model_name = self.kwargs['model_name']
+        model_class = apps.get_model(app_name, model_name)
+        return model_class.objects.all()
 
     def get_serializer_class(self):
         pass
