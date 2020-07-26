6e36acfa505ddfaa963e629b94160992035fc2e1
ernest <wolf1080@yandex.ru>
2020-07-26 14:46:15 +0300

    init other tests

--- tests/tests.py
+++ tests/tests.py
@@ -22,3 +22,30 @@ class TestModelApi:
 
         result_data = [{'id': 1, 'name': 'Charlie', 'age': 5}]
         assert response.json() == result_data
+
+    def test_create_dog(self, client):
+        pass
+
+    def test_retrieve_dog(self, client):
+        pass
+
+    def test_retrieve_non_existent_dog(self, client):
+        pass
+
+    def test_update_dog(self, client):
+        pass
+
+    def test_delete_dog(self, client):
+        pass
+
+    def test_filter_dogs(self, client):
+        pass
+
+    def test_asc_ordering_dogs(self, client):
+        pass
+
+    def test_desc_ordering_dogs(self, client):
+        pass
+
+    def test_paginate_dogs(self, client):
+        pass
